eea6e469f299d2606e045af9d41427022b70deac
jyhyun1008 <hyun1008@kasi.re.kr>
2022-12-17 08:56:16 +0900

    add pages

---
+++ js/mdparser.js
@@ -0,0 +1,90 @@
+function parseMd(md){
+  
+    //ul
+    md = md.replace(/^\s*\n\*/gm, '<ul>\n*');
+    md = md.replace(/^(\*.+)\s*\n([^\*])/gm, '$1\n</ul>\n\n$2');
+    md = md.replace(/^\*(.+)/gm, '<li>$1</li>');
+    
+    //ol
+    md = md.replace(/^\s*\n\d\./gm, '<ol>\n1.');
+    md = md.replace(/^(\d\..+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2');
+    md = md.replace(/^\d\.(.+)/gm, '<li>$1</li>');
+    
+    //blockquote
+    md = md.replace(/^\>(.+)/gm, '<blockquote>$1</blockquote>');
+
+    //hr
+    md = md.replace(/[\-]{3}/g, '</div></div><div class="item_wrap"><div class="item">');
+    
+    //h
+    md = md.replace(/\n[\#]{6}(.+)/g, '<h6>$1</h6>');
+    md = md.replace(/\n[\#]{5}(.+)/g, '<h5>$1</h5>');
+    md = md.replace(/\n[\#]{4}(.+)/g, '<h4>$1</h4>');
+    md = md.replace(/\n[\#]{3}(.+)/g, '<h3>$1</h3>');
+    md = md.replace(/\n[\#]{2}(.+)/g, '<h2>$1</h2>');
+    md = md.replace(/\n[\#]{1}(.+)/g, '</div></div><div class="item_wrap"><div class="item"><h1>🎥 $1</h1>');
+    
+    //images
+    md = md.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1" />');
+    
+    //links
+    md = md.replace(/[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<a href="$2" title="$4">$1</a>');
+    
+    //font styles
+    md = md.replace(/[\*]{2}([^\*]+)[\*]{2}/g, '<strong>$1</strong>');
+    md = md.replace(/[\*]{1}([^\*]+)[\*]{1}/g, '<i>$1</i>');
+    md = md.replace(/[\~]{2}([^\~]+)[\~]{2}/g, '<del>$1</del>');
+    
+    //pre
+    md = md.replace(/^\s*\n\`\`\`(([^\s]+))?/gm, '<pre class="code" id="$2">');
+    md = md.replace(/^\`\`\`\s*\n/gm, '</pre>\n\n');
+    
+    //code
+    md = md.replace(/[\`]{1}([^\`]+)[\`]{1}/g, '<code>$1</code>');
+
+    //주석
+    md = md.replace(/\n[\/]{2}(.+)/g, '');
+
+    //br
+    md = md.replace(/\n\n/g, '</p><p>');
+    
+    return md;
+    
+  }
+
+function getQueryStringObject() {
+    var a = window.location.search.substr(1).split('&');
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+        var p = a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+}
+  
+var qs = getQueryStringObject();
+var post = qs.post;
+
+if (!post) {
+    var folderurl = "https://raw.githubusercontent.com/jyhyun1008/VN.MD/main/README.md"
+    fetch(folderurl)
+    .then(res => res.text())
+    .then((out) => {
+        document.querySelector("#post").innerHTML += parseMd(out);
+    })
+    .catch(err => { throw err });
+} else {
+    var url = "https://raw.githubusercontent.com/jyhyun1008/jyhyun1008.github.io/main/post/"+post+".md";
+    fetch(url)
+    .then(res => res.text())
+    .then((out) => {
+        document.querySelector(".title").innerText = post
+        document.querySelector("#post").innerHTML += parseMd(out)
+    })
+    .catch(err => { throw err });
+}
+
