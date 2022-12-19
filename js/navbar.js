351388ee33fbfca643c8713e220d71b7d4fcf6d8
Hyun, Jaeyeon <93899740+jyhyun1008@users.noreply.github.com>
2022-12-19 10:10:55 +0900

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,8 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+document.documentElement.style.setProperty('--accent', `${theme}px`);
+
 // resize
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
