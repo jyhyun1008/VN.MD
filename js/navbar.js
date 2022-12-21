6476e3aa14d9a82e8199b5969c951c5221712eac
Hyun, Jaeyeon <93899740+jyhyun1008@users.noreply.github.com>
2022-12-22 06:13:16 +0900

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -19,11 +19,13 @@ logoText.innerHTML = "<a href='./'>" + logo + "</a>"
 // ----- 모바일 뷰포트 계산 -----
 
 let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 // 리사이즈
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 })
 
