94fb2eca20f83eb8634e98e19eb580e654056410
Hyun, Jaeyeon <93899740+jyhyun1008@users.noreply.github.com>
2022-12-22 06:35:25 +0900

    Update effects.js

--- js/effects.js
+++ js/effects.js
@@ -1,3 +1,4 @@
+//----- 배경 이펙트 -----
 
 function grayScale(){
   bg.style.filter = "grayscale(90%)";
@@ -25,4 +26,69 @@ function none(){
   document.querySelector('#chr0').style.filter = "none";
   document.querySelector('#chr1').style.filter = "none";
   document.querySelector('#chr2').style.filter = "none";
+}
+
+//----- 캐릭터 이펙트(주로 애니메이션) -----
+
+function moveToCenter(chrId){
+  var startP = document.querySelector(chrId).style.left;
+  var endP = 50*vw - 200;
+  
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    document.querySelector(chrId).style.left = (startP + (endP - startP) * progress/1000) + 'px';
+    if (progress < 1000) {
+      window.requestAnimationFrame(step);
+    }
+  }
+  window.requestAnimationFrame(step);
+}
+
+function passToRight(chrId){
+  var startP = -400;
+  var endP = 100*vw;
+  
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    document.querySelector(chrId).style.left = (startP + (endP - startP) * progress/1000) + 'px';
+    if (progress < 1000) {
+      window.requestAnimationFrame(step);
+    }
+  }
+  window.requestAnimationFrame(step);
+}
+
+function passToLeft(chrId){
+  var startP = 100*vw;
+  var endP = -400;
+  
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    document.querySelector(chrId).style.left = (startP + (endP - startP) * progress/1000) + 'px';
+    if (progress < 1000) {
+      window.requestAnimationFrame(step);
+    }
+  }
+  window.requestAnimationFrame(step);
+}
+
+function jump(chrId){
+  var startP = document.querySelector(chrId).style.top;
+  var speed = 10px;
+  
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    document.querySelector(chrId).style.top = (document.querySelector(chrId).style.top + (speed - g) * progress/1000) + 'px';
+    g++;
+    if (document.querySelector(chrId).style.top < 1) {
+      window.requestAnimationFrame(step);
+    } else {
+      document.querySelector(chrId).style.top = startP;
+  }
+  var g = 0;
+  window.requestAnimationFrame(step);
 }
