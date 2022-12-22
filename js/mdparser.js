efa624a19918ab6d43d0a4db74dcce5000ee6bdc
Hyun, Jaeyeon <93899740+jyhyun1008@users.noreply.github.com>
2022-12-23 06:26:22 +0900

    Update mdparser.js

--- js/mdparser.js
+++ js/mdparser.js
@@ -8,6 +8,11 @@ function parseMd(md){
     md = md.replace(/^(\*\s.+)\s*\n([^\*])/gm, '$1\n</ul>\n\n$2');
     md = md.replace(/^\*\s(.+)/gm, '<li>$1</li>');
     
+    //ul
+    md = md.replace(/^\s*\n\-\s/gm, '<ul>\n* ');
+    md = md.replace(/^(\-\s.+)\s*\n([^\-])/gm, '$1\n</ul>\n\n$2');
+    md = md.replace(/^\-\s(.+)/gm, '<li>$1</li>');
+    
     //ol
     md = md.replace(/^\s*\n\d\.\s/gm, '<ol>\n1. ');
     md = md.replace(/^(\d\.\s.+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2');
